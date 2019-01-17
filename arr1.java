b74d1be8e9f312164ea22dc6050bba05d3b88967
YashRajSaxena101 <46774742+YashRajSaxena101@users.noreply.github.com>
2019-01-17 15:47:51 +0530

    Add files via upload

---
+++ arr1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class arr1
+{
+    public static void main()
+    {
+        System.out.println("enter the length of array");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+    }
+}
+        
